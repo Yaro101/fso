0af63c9de69ced11500010bb57d56ad6334b4915
Yahya Rouissi <yahyarouissi@gmail.com>
2024-06-06 17:57:39 +0300

    2.1-2.5

---
+++ osa2/kurssitiedot-2/2-1/src/Course.jsx
@@ -0,0 +1,45 @@
+const Header = (props) => {
+  return <h1>{props.course.name}</h1>;
+};
+
+const Total = (props) => {
+  return (
+    <p>
+      <b>Number of exercises {props.sumOfExercises}</b>
+    </p>
+  );
+};
+
+const Part = (props) => {
+  return (
+    <p>
+      {props.part.name} {props.part.exercises}
+    </p>
+  );
+};
+
+const Content = (props) => {
+  return (
+    <>
+      {props.parts.map((part) => (
+        <Part key={part.id} part={part} />
+      ))}
+    </>
+  );
+};
+const Course = (props) => {
+  const sumOfExercises = props.course.parts.reduce(
+    (sum, part) => sum + part.exercises,
+    0
+  );
+
+  return (
+    <div id={props.course.id}>
+      <Header course={props.course} />
+      <Content parts={props.course.parts} />
+      <Total sumOfExercises={sumOfExercises} />
+    </div>
+  );
+};
+
+export default Course;
